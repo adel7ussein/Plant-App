facfc7b2ed3c39f1b3c517515b274a9112a71b73
adel7ussein <aadelshabib67009@gmail.com>
2024-02-28 04:06:12 +0200

    add a textfield as a search bar

--- lib/screens/home/component/body.dart
+++ lib/screens/home/component/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plant_app/constants.dart';
 
 class Body extends StatelessWidget {
@@ -22,13 +23,45 @@ class Body extends StatelessWidget {
                       bottomLeft: Radius.circular(30),
                       bottomRight: Radius.circular(30))),
             ),
-            Positioned(child: Container(
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+                child: Container(
               margin: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
               height: 54,
-              decoration: const BoxDecoration(
-                color: Colors.white
-              ),
-            ))
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(20),
+                  boxShadow:  [
+                    BoxShadow(
+                      offset: const Offset(0, 10),
+                      blurRadius: 50,
+                      color: kPrimaryColor.withOpacity(0.23)
+                    )
+                  ]),
+                  child:  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            onChanged: (value){},
+                            decoration: InputDecoration(
+                              hintText: "Search",
+                              hintStyle: TextStyle(
+                                color: kPrimaryColor.withOpacity(0.5)
+                              ),
+                              enabledBorder: InputBorder.none,
+                              focusedBorder: InputBorder.none
+                            ),
+                          ),
+                        ),
+                        SvgPicture.asset("assets/icons/search.svg")
+                      ],
+                    ),
+                  ),
+            )
+            )
           ]),
         )
       ],
