2f2b308277717f6c806ead70738c59d2eb222b50
adel7ussein <aadelshabib67009@gmail.com>
2024-03-02 02:58:44 +0200

    create detail screen

--- lib/screens/home/component/recommended_listof_plants.dart
+++ lib/screens/home/component/recommended_listof_plants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app/screens/details/details_screen.dart';
 import 'package:plant_app/screens/home/component/custom_recommend_plantcard.dart';
 
 class RecommendedListOfPlants extends StatelessWidget {
@@ -17,21 +18,36 @@ class RecommendedListOfPlants extends StatelessWidget {
             title: "Samantha",
             country: "America",
             price: 440,
-            press: () {},
+            press: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const DetailScreen()));
+            },
           ),
           CustomRecommendPlantCard(
             image: "assets/images/image_2.png",
             title: "Samantha",
             country: "Russia",
             price: 440,
-            press: () {},
+            press: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const DetailScreen()));
+            },
           ),
           CustomRecommendPlantCard(
             image: "assets/images/image_3.png",
             title: "Samantha",
             country: "China",
             price: 440,
-            press: () {},
+            press: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const DetailScreen()));
+            },
           ),
         ],
       ),
